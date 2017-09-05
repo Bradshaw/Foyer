da4e0fab3c40456a4c400ad67552f4bb4645b720
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-09-05 23:22:38 +0200

    Simplest

---
+++ rotonde/rotonde.js
@@ -0,0 +1,35 @@
+var rotonde = {
+  get: function() {
+    return {
+      profile: {
+        name: "Gaeel",
+        location: "Abstract",
+        position: "0, 0",
+        color: "#DDEEEE",
+        avatar: "https://pbs.twimg.com/profile_images/879291279292628992/7hCKAdcY_400x400.jpg"
+      },
+      feed:[
+        {
+          time: 0,
+          text: "Hello, world!",
+          topic: "Testing",
+          task: "Rotonde",
+          data: {
+            focus: 0.1,
+            sleep: 0,
+            nutrition: 0.2
+          }
+        }
+      ],
+      portal: [
+        "rotonde.v-os.ca",
+        "rotonde.xxiivv.com"
+      ]
+    }
+  },
+}
+
+
+
+
+module.exports = rotonde;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var rotonde = require('../rotonde/rotonde')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (req.headers.hasOwnProperty("accept") && req.headers.accept.includes("text/html"))
+    res.render('rotonde', rotonde.get());
+  else
+    res.send(JSON.stringify(rotonde.get()))
 });
 
 module.exports = router;

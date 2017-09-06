b3fe39b402cf11e129b0768b5c6277988fc82a76
Kevin Bradshaw <gaeel@nanoleptic.net>
2017-09-06 10:25:43 +0200

    Added redirect to promenade

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
-var rotonde = require('../rotonde/rotonde')
+var rotonde = require('../rotonde/rotonde');
+var promenadePath = "http://promenade.nanoleptic.net/";
+var localPath = "rotonde.nanoleptic.net";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (req.headers.hasOwnProperty("accept") && req.headers.accept.includes("text/html"))
-    res.render('rotonde', rotonde.get());
-  else
+  if (req.headers.hasOwnProperty("accept") && req.headers.accept.includes("text/html")){
+    res.writeHead(302, {
+      'Location': promenadePath+localPath
+    });
+    res.end();
+  } else {
     res.send(JSON.stringify(rotonde.get()))
+  }
 });
 
 module.exports = router;
